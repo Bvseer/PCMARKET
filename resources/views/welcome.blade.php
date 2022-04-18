863c2e14ad50f9c7d5538a8d541e31572d3b500d
Basir Xujamuradov <BXujamuradov@paymart.local>
2022-04-18 17:10:14 +0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>PC MARKET</title>
+
+    </head>
+    <body class="antialiased">
+        <h1>PC SHOP</h1>
+    </body>
+</html>
